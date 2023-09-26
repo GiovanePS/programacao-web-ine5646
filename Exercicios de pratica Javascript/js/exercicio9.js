29506f42a947586c03ecca882c61b5bf5f756a82
GiovanePS <57882319+GiovanePS@users.noreply.github.com>
2023-09-25 21:39:51 -0300

    Add files via upload

--- Exercicios de pratica Javascript/js/exercicio9.js
+++ Exercicios de pratica Javascript/js/exercicio9.js
@@ -1,26 +1,98 @@
+
 function haOnzeDigitos(cpf) {
     //---- edite aqui para a validação do exercício 9a
-    return false
+    let strCPF = String(cpf).replace(/[^\d]/g, '')
+    if (strCPF.length !== 11)
+        return false
+    else {
+        return true
+    }
 }
 
 function todosOsOnzeDigitosSaoNumeros(cpf) {
     //---- edite aqui para a validação do exercício 9b
-    return false
+    let strCPF = String(cpf).replace(/[^\d]/g, '')
+      if ([
+    '00000000000',
+    '11111111111',
+    '22222222222',
+    '33333333333',
+    '44444444444',
+    '55555555555',
+    '66666666666',
+    '77777777777',
+    '88888888888',
+    '99999999999',
+    ].indexOf(strCPF) !== -1) {
+        return false
+    } else {
+        return true
+    }
+    
 }
 
 function osOnzeNumerosSaoDiferentes(cpf) {
     //---- edite aqui para a validação do exercício 9c
-    return false
+    let strCPF = String(cpf).replace(/[^\d]/g, '')
+
+    let numeros = [
+    '00000000000',
+    '11111111111',
+    '22222222222',
+    '33333333333',
+    '44444444444',
+    '55555555555',
+    '66666666666',
+    '77777777777',
+    '88888888888',
+    '99999999999',
+    ]
+
+    numeros.forEach(element => {
+        if (element == strCPF) {
+            return false
+        }
+    });
+
+    return true
 }
 
 function oPrimeiroDigitoVerificadorEhValido(cpf) {
     //---- edite aqui para a validação do exercício 9d
-    return false
+  let strCPF = String(cpf).replace(/[^\d]/g, '');
+
+  if (strCPF.length !== 11) {
+    return false;
+  }
+
+  let soma = 0;
+  for (let i = 0; i < 9; i++) {
+    soma += parseInt(strCPF.charAt(i)) * (10 - i);
+  }
+
+  let resto = soma % 11;
+  let digitoVerificador = resto < 2 ? 0 : 11 - resto;
+
+  return digitoVerificador === parseInt(strCPF.charAt(9));
 }
 
 function oSegundoDigitoVerificadorEhValido(cpf) {
     //---- edite aqui para a validação do exercício 9e
-    return false
+  let strCPF = String(cpf).replace(/[^\d]/g, '');
+
+  if (strCPF.length !== 11) {
+    return false;
+  }
+
+  let soma = 0;
+  for (let i = 0; i < 10; i++) {
+    soma += parseInt(strCPF.charAt(i)) * (11 - i);
+  }
+
+  let resto = soma % 11;
+  let digitoVerificador = resto < 2 ? 0 : 11 - resto;
+
+  return digitoVerificador === parseInt(strCPF.charAt(10));
 }
 
 
